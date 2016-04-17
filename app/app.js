78a49af81f24c987f840f74a981f96eb7f660984
mla23 <mla23@njit.edu>
2016-04-17 16:36:32 -0400

    changing display of views
    changing views display

--- app/app.js
+++ app/app.js
@@ -9,13 +9,13 @@ angular.module('myApp',['myApp.controllers',
 //config() lets us configure a module
 
 
-angular.module('myApp').config(function($routeProvider){
+angular.module('myApp').config(function($routeProvider,$locationProvider){
     $routeProvider.when('/view1',
         {
             controller: 'Controller1',
             templateUrl: 'partials/view1.html',
         }).when('/view2',{
             controller:'Controller2',
-        templateUrl:'partials/view2.html',
-    });
+        templateUrl:'partials/view2.html'}).otherwise({redirectTo:'/view1'});
+    $locationProvider.html5Mode(True);//activate HTML5 MODE
         });
